d2a4e6c86ce8d2495bd85f4149e41afffce6b6ca
Minh Nhat Phan <69678769+mnhat64@users.noreply.github.com>
2023-09-24 14:54:11 +0200

    Java Testaufgabe

---
+++ src/main/java/org/example/ImportEdgeHelper.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.gradoop.flink.io.impl.graph.tuples.ImportEdge;
+
+import java.util.UUID;
+
+public class ImportEdgeHelper {
+    public static DataSet<ImportEdge<String>> createEdgesFromCSV(
+            DataSet<Tuple4<String, String, String, String>> edgeCSV) {
+
+        return edgeCSV.map(new MapFunction<Tuple4<String, String, String, String>, ImportEdge<String>>() {
+            @Override
+            public ImportEdge<String> map(Tuple4<String, String, String, String> csvRecord) throws Exception {
+                String fromId = csvRecord.f0;
+                String toId = csvRecord.f1;
+                String relation = "Guaranteed: " + csvRecord.f3;
+                String edgeId = UUID.randomUUID().toString();
+                return new ImportEdge<>(edgeId, fromId, toId, relation);
+            }
+        });
+    }
+}
